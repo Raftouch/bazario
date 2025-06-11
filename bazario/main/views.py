8695e539a3c506149d258bf1cee9a51bbd145f8a
raftouch <rafanelkin@gmail.com>
2025-06-11 17:31:39 +0200

    add views

--- bazario/main/views.py
+++ bazario/main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def featured_products(request):
+    products = Product.objects.filter(available=True)[:3]
+    return render(request, 'main/index/index.html', {'products': products})
+
+
+def product_details(request, slug):
+    product = get_object_or_404(Product, slug=slug, available=True)
+    return render(request, 'main/product/details.html', {'product': product})
