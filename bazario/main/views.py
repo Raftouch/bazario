13a619730042632d6e5d809181857d0f51ed38ab
raftouch <rafanelkin@gmail.com>
2025-06-12 18:48:17 +0200

    create product_list view

--- bazario/main/views.py
+++ bazario/main/views.py
@@ -10,3 +10,13 @@ def featured_products(request):
 def product_details(request, slug):
     product = get_object_or_404(Product, slug=slug, available=True)
     return render(request, 'main/product/details.html', {'product': product})
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'main/product/list.html', {'category': category, 'categories': categories, 'products': products})
