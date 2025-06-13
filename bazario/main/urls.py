8aa7fb0bff18cda3da8c0872d8e016637ebbe8a5
raftouch <rafanelkin@gmail.com>
2025-06-13 15:22:01 +0200

    add url paths to display views

--- bazario/main/urls.py
+++ bazario/main/urls.py
@@ -6,5 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.featured_products, name='featured_products'),
-    path('<slug:slug>/', views.product_details, name='product_details'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('products/<slug:slug>/', views.product_details, name='product_details'),
 ]
