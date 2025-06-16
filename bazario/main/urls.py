1f558ffd24a2dda601debcf469555c53442eed7d
raftouch <rafanelkin@gmail.com>
2025-06-16 20:33:36 +0200

    add pagination

--- bazario/main/urls.py
+++ bazario/main/urls.py
@@ -7,6 +7,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.product_list, name='product_list'),
-    path('products/category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('products/<slug:slug>/', views.product_details, name='product_details'),
+    path('products/category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
 ]
--- bazario/main/views.py
+++ bazario/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Category, Product
 
 
@@ -16,7 +17,17 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+
+    page = request.GET.get('page', 1)
+    p = Paginator(products, 1)
+    current_page_products = p.page(int(page))
+
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'main/product/list.html', {'category': category, 'categories': categories, 'products': products})
+        p = Paginator(products, 1)
+        current_page_products = p.page(int(page))
+        
+    return render(request, 
+                  'main/product/list.html', 
+                  {'category': category, 'categories': categories, 'products': current_page_products, 'slug_url': category_slug})
