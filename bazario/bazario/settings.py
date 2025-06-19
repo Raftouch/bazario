9f032ff07bc3a001bdc59331816987a01b65d44c
raftouch <rafanelkin@gmail.com>
2025-06-19 14:47:56 +0200

    register cart in installed apps

--- bazario/bazario/settings.py
+++ bazario/bazario/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'main',
+    'cart',
     'tailwind',
     'theme',
     'django_browser_reload'
